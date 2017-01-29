4c0974cc1093a5183dbe1d04c7501cf9326f03e3
parthsr <srivastavaparth2016@gmail.com>
2017-01-29 18:47:56 +0530

    Add files via upload

---
+++ WeatherBot/Weather.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace WeatherBot
+{
+    class WeatherThings
+    {
+        public static String[] Weather(String location)
+        {
+            String desc="", temp="";String ImageUrl="";
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid=680aaaa5258e15d43253263b8c9da7f3",
+                location);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                if (json.Contains("visibility"))
+                {
+
+                    Rootobject weatherInfo = (new JavaScriptSerializer()).Deserialize<Rootobject>(json);
+                    desc = weatherInfo.weather[0].description;
+                    ImageUrl = string.Format("http://openweathermap.org/img/w/{0}.png",
+                         weatherInfo.weather[0].icon);
+                    temp = weatherInfo.main.temp.ToString();
+                }
+                else {
+                    Rootobject1 weatherInfo = (new JavaScriptSerializer()).Deserialize<Rootobject1>(json);
+                    desc = weatherInfo.weather[0].description;
+                    ImageUrl = string.Format("http://openweathermap.org/img/w/{0}.png",
+                         weatherInfo.weather[0].icon);
+                    temp = weatherInfo.main.temp.ToString();
+
+                }
+            }
+            string[] final = new string[2];
+
+            final[0] = "TEMP: " + temp + " DESCRIPTION: " + desc;
+            final[1] = ImageUrl;
+            return final;
+        }
+    }
+}
---
+++ WeatherBot/WeatherInfo1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherBot
+{
+
+
+    public class Coord1
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Weather1
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main1
+    {
+        public double temp { get; set; }
+        public double pressure { get; set; }
+        public int humidity { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double sea_level { get; set; }
+        public double grnd_level { get; set; }
+    }
+
+    public class Wind1
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+
+    public class Clouds1
+    {
+        public int all { get; set; }
+    }
+
+    public class Sys1
+    {
+        public double message { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Rootobject1
+    {
+        public Coord1 coord{ get; set; }
+        public List<Weather1> weather { get; set; }
+        public string @base { get; set; }
+        public Main1 main { get; set; }
+        public Wind1 wind { get; set; }
+        public Clouds1 clouds { get; set; }
+        public int dt { get; set; }
+        public Sys1 sys { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
+
+}
+
